ced3640ca139f69985e6980b7e4b547e9e2b4896
Joey Walton <joeydw10@hotmail.com>
2022-09-02 14:23:23 -0400

    Test

--- C++/OOP244/Workshops/WorkshopZero/prg.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-	cout << "Fardad Was here" << endl;
-	for (int i = 0; i < 10; i++) {
-		cout << (i+1) <<  "- Hello I am Joey Walton" << endl;
-	}
-	return 0;
-}
